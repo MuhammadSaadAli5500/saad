5b6c6ccce9764181b11cf21ebda31d21f9db6f60
Saad Ali <msaadali602@gmail.com>
2022-09-13 11:22:47 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/saad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.saad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/jjj.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyAPP extends StatelessWidget {
+  const MyAPP({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
